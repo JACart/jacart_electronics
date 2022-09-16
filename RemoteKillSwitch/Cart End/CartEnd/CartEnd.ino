6f1cf76834cc646471075a1684a5243269d877d2
Jason Forsyth <forsy2jb@jmu.edu>
2022-09-16 15:50:40 -0400

    Add LEDs for state

--- RemoteKillSwitch/Cart End/CartEnd/CartEnd.ino
+++ RemoteKillSwitch/Cart End/CartEnd/CartEnd.ino
@@ -17,15 +17,12 @@
 
    ---------------------
 
-   Code is re-organized to better separate state and action logic. 
+   Code is re-organized to better separate state and action logic.
 
    Board is Arduino Uno 5V
 
    Jason Forsyth - 7/12/2022
 */
-
-//whether to be overly verbose with debug messages
-#define DEBUG 1
 
 //include SoftwareSerial to remove Xbee from built-in RX and TX lines
 #include <SoftwareSerial.h>
@@ -37,33 +34,38 @@ const int XBEE_RX_PIN = 2;
 const int XBEE_TX_PIN = 3;
 
 //instantiate SoftwareSerial for Xbee
-SoftwareSerial Xbee (XBEE_RX_PIN,XBEE_TX_PIN);
+SoftwareSerial Xbee (XBEE_RX_PIN, XBEE_TX_PIN);
 
 // I/O pin for controlling relay
 #define RELAY 7
 
 /**
- * Message formats for the RUN and KILL commands
- */
+   Message formats for the RUN and KILL commands
+*/
 const char RUN_MSG = '1';
 const char KILL_MSG = '0';
 
 // time stamp in millis for last packet received
 unsigned long lastPacketReceived = 0;
 
-// Timeout in milliseconds.  Relay will flip if data is not received for period of time. 
-unsigned long TIMEOUT = 2000;  
+// Timeout in milliseconds.  Relay will flip if data is not received for period of time.
+unsigned long TIMEOUT = 2000;
 
 /**
- * Various states for the Cart's operation
- * DISCONNECTED: cart is disconnected from radio and timeout has exceeded. Should kill.
- * CONNECTED_RUN: cart is connected to radio and last received command was RUN. Should run.
- * CONNECTED_KILL: cart is connected to radio and last received command was KILL. Should kill.
- * TIMEOUT_PENDING: cart is disconnected from radio, but has not yet timed out. Should repeat last command.
- */
+   Various states for the Cart's operation
+   DISCONNECTED: cart is disconnected from radio and timeout has exceeded. Should kill.
+   CONNECTED_RUN: cart is connected to radio and last received command was RUN. Should run.
+   CONNECTED_KILL: cart is connected to radio and last received command was KILL. Should kill.
+   TIMEOUT_PENDING: cart is disconnected from radio, but has not yet timed out. Should repeat last command.
+*/
 enum STATE {STATE_DISCONNECTED, STATE_CONNECTED_RUN, STATE_CONNECTED_KILL, STATE_TIMEOUT_PENDING};
 enum COMMAND {CMD_RUN, CMD_KILL, CMD_NONE};
 
+//indicator RGB LED pins
+const int LED_RED = A0;
+const int LED_GROUND = A1;
+const int LED_GREEN = A2;
+const int LED_BLUE = A3;
 
 void setup() {
 
@@ -71,11 +73,16 @@ void setup() {
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
 
-  //utilize built-in LED
-  pinMode(LED_BUILTIN,OUTPUT);
+  //use built-in RED LED to indicate cart state
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GROUND, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
 
-  //set default to off
-  digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GROUND, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, LOW);
 
   //setup communication with Xbeee
   Xbee.begin(9600);
@@ -88,8 +95,8 @@ void setup() {
 }
 
 /**
- * State variables to manage board operation
- */
+   State variables to manage board operation
+*/
 enum STATE current_state = STATE_DISCONNECTED;
 enum STATE next_state = STATE_DISCONNECTED;
 
@@ -98,13 +105,13 @@ enum COMMAND last_good_command = CMD_NONE;
 
 //enable cart operation
 inline void run_cart()
-{        
+{
 
   //enable relay
   digitalWrite(RELAY, HIGH);
 
   //turn on built-in LED
-  digitalWrite(LED_BUILTIN,HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 //disable cart operation
@@ -114,7 +121,7 @@ inline void kill_cart()
   digitalWrite(RELAY, LOW);
 
   //turn off built-in LED
-  digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 //runs forever
@@ -129,26 +136,37 @@ void loop() {
   {
     run_cart();
 
-    #if DEBUG
+#if DEBUG
     Serial.println("RUN!");
-    #endif
-    
+#endif
+
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, LOW);
+
   }
   else if (current_state == STATE_DISCONNECTED)
   {
     kill_cart();
 
-    #if DEBUG
+#if DEBUG
     Serial.println("DISC!");
-    #endif
+#endif
+
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_BLUE, HIGH);
+
   }
   else if (current_state == STATE_CONNECTED_KILL)
   {
     kill_cart();
 
-    #if DEBUG
     Serial.println("KILL!");
-    #endif
+
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_BLUE, LOW);
   }
   else if (current_state == STATE_TIMEOUT_PENDING)
   {
@@ -163,9 +181,13 @@ void loop() {
     else //failsafe is last command was CMD_NONE
     {
       kill_cart();
-      #if DEBUG
+
       Serial.println("FAILSAFE!");
-      #endif
+
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_BLUE, HIGH);
+
     }
   }
 
@@ -173,9 +195,11 @@ void loop() {
   else
   {
     kill_cart();
-    #if DEBUG
     Serial.println("FAILSAFE!");
-    #endif
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+
   }
 
 
@@ -193,11 +217,14 @@ void loop() {
     //read a byte
     char c = Xbee.read();
 
+    //print out to serial terminal
+    Serial.println(c);
+
     // if a valid command was received
     if (c == RUN_MSG || c == KILL_MSG)
     {
       //if command was RUN, assign CMD_RUN; otherwise CMD_KILL
-      received_command = (c==RUN_MSG)?CMD_RUN:CMD_KILL;
+      received_command = (c == RUN_MSG) ? CMD_RUN : CMD_KILL;
 
       //mark valid packet received
       valid_packet_recevied = true;
@@ -251,22 +278,20 @@ void loop() {
     {
       next_state = STATE_DISCONNECTED;
     }
-    
+
     //timeout has expired. Go to disconnected.
     else
     {
       next_state = STATE_DISCONNECTED;
 
-      #if DEBUG
       Serial.println("TIMEOUT!");
-      #endif
     }
   }
 
   //failsafe state because our logic is wrong
   else
   {
-    
+
     next_state = STATE_DISCONNECTED;
   }
 
@@ -275,34 +300,4 @@ void loop() {
 
   delay(50);
 
-  /*
-
-    if (currentTime - lastPacketReceived > TIMEOUT) {
-      digitalWrite(RELAY, LOW);
-      lastPacketReceived = currentTime;
-    }
-    while (Xbee.available() > 0) {
-      currentTime = millis();
-
-      // Disconnect failsafe.  Times out if data is not received for TIMEOUT
-      if (currentTime - lastPacketReceived > TIMEOUT) {
-        digitalWrite(RELAY, LOW);
-        lastPacketReceived = currentTime;
-      }
-
-      // Gets Xbee data and converts to an integer value
-      char c = Xbee.read();
-      int val = c - '0';
-      if (val != currentState) {
-        // Relay state triggers
-        if (val == 0) {
-          digitalWrite(RELAY, LOW);
-        }
-        if (val == 1) {
-          digitalWrite(RELAY, HIGH);
-        }
-        currentState = val;
-      }
-      lastPacketReceived = currentTime;
-    } */
 }
--- RemoteKillSwitch/User End/UserEnd/UserEnd.ino
+++ RemoteKillSwitch/User End/UserEnd/UserEnd.ino
@@ -35,10 +35,19 @@ SoftwareSerial Xbee (XBEE_RX_PIN,XBEE_TX_PIN);
 //GPIO pin that is connected to the switch
 #define SWITCH 12
 
+//message to send across the network
 const char RUN_MSG = '1';
 const char KILL_MSG = '0';
 
+//variable for tracking switch state
 int switch_state = 0;
+
+//indicator RGB LED pins
+const int LED_RED = A0;
+const int LED_GROUND = A1;
+const int LED_GREEN = A2;
+const int LED_BLUE = A3;
+
 
 void setup() {
   Serial.begin(9600);
@@ -46,8 +55,16 @@ void setup() {
   pinMode(SWITCH, INPUT_PULLUP);
 
   //use built-in RED LED to indicate switch state
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(LED_RED,OUTPUT);
+  pinMode(LED_GROUND,OUTPUT);
+  pinMode(LED_GREEN,OUTPUT);
+  pinMode(LED_BLUE,OUTPUT);
+
+  digitalWrite(LED_RED,LOW);
+  digitalWrite(LED_GROUND,LOW);
+  digitalWrite(LED_GREEN,LOW);
+  digitalWrite(LED_BLUE,LOW);
+  
 }
 
 void loop() {
@@ -64,8 +81,10 @@ void loop() {
     //send run message as debug up serial port
     Serial.println("Run!");
 
-    //turn RED LED on
-    digitalWrite(LED_BUILTIN, HIGH);
+    //turn GREEN LED on, all others OFF
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN,HIGH);
+    digitalWrite(LED_BLUE,LOW);
 
   } 
   else {
@@ -76,8 +95,10 @@ void loop() {
     //send kill message as debug up serial port 
     Serial.println("Kill!");
 
-    //turn RED LED off
-    digitalWrite(LED_BUILTIN, LOW);
+    //turn RED ON off, all others OFF
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN,LOW);
+    digitalWrite(LED_BLUE,LOW);
   }
 
   //loop 4x a second
